a90bd32193f37df14579f42e00e0555a440cb12c
QuinnSuligoy <56174399+QuinnSuligoy@users.noreply.github.com>
2019-12-11 13:55:47 -0800

    CanSee

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,15 +6,38 @@ public class Enemy : MonoBehaviour
 {
     public GameObject Player;
     public GameObject Lazer;
+    public bool CanSee;
+    public GameObject Blaster;
+    public  int speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CanSee = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Turn The Blaster
+        if(CanSee == true)
+        {
+           
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            CanSee = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            CanSee = false;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Get inputs
         horzInput = Input.GetAxis("Horizontal");
